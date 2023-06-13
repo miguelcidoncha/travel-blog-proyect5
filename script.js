8852a2b59d3d1954dacee2c5bcfa9be437ee41b4
miguelcidoncha <cidoncha1996@gmail.com>
2023-06-13 13:33:32 +0200

    Desplegable

---
+++ script.js
@@ -0,0 +1,5 @@
+let discoverE = document.getElementById("discover-E");
+discoverE.addEventListener("mouseleave", function() {
+    let dropdownContent = document.getElementById("dropdown-content");
+    dropdownContent.style.display = "none";
+});
