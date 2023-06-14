247d62788fd8b0b40c28e9485f30792bd1ec5267
miguelcidoncha <cidoncha1996@gmail.com>
2023-06-14 12:50:31 +0200

    add.leatlef.map

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let discoverE = document.getElementById("discover-E");
 let discoverS = document.getElementById("discover-S");
-discoverE.addEventListener("mouseleave", function() {
+discoverE.addEventListener("mouseleave", function () {
     let dropdownContentE = document.getElementById("dropdown-content-E");
     let dropdownContentS = document.getElementById("dropdown-content-S");
     dropdownContentE.style.display = "none";
